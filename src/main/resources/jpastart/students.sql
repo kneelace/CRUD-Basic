10df6857f062a724d188cd6c919025f39a0ccdc4
NIL35H <2015nilesh.bhatia@ves.ac.in>
2022-06-24 15:50:19 +0530

    Added SQL Queries for Basic JPA starter Crud Operation

---
+++ src/main/resources/jpastart/students.sql
@@ -0,0 +1,9 @@
+DROP TABLE students_jpa;
+CREATE TABLE students_jpa
+(studentId NUMBER PRIMARY key,
+studentName VARCHAR2(30));
+
+
+insert into students_jpa values(111,'ABCD');
+
+select * from STUDENTS_JPA;
